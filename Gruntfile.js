ee6a2709b1f2c72626716a85ac8851b61e5a1ff3
Luisa Hurtado <luisah@users.noreply.github.com>
2014-09-20 22:56:00 -0700

    Setting up JSHint, Uglify, Concat, Connect, Watch, Compass, with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      files: {
+        src: ['src/**.js'],
+      },
+      options: {
+        globals: {
+
+        }
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'src/scss',
+          cssDir: 'dist/css',
+          environment: 'production'
+        }
+      },
+      dev: {
+        options: {
+          sassDir: 'src/scss',
+          cssDir: 'dist/css',
+          environment: 'development'
+        }
+      }
+    },
+    watch: {
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: ['dist/**/*.css'],
+      },
+      scripts: {
+        files: ['src/**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+        },
+      },
+      css: {
+        files: 'src/scss/*.scss',
+        tasks: ['compass:dev'],
+      },
+    },
+    connect: {
+      uses_defaults: {}
+    },
+    concat: {
+      prod: {
+        files: {
+          'dist/main.js': ['src/main.js'],
+          // 'dist/with_extras.js': ['src/main.js', 'src/extras.js'],
+        }
+      }
+    },
+    uglify: {
+      prod: {
+        options: {
+          sourceMap: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/js',
+          src: 'src/*.js',
+          dest: 'dist/js'
+        }]
+      }
+    }
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  // Dev tasks
+  grunt.registerTask('default', ['connect', 'watch']);
+
+  // Prod task(s).
+  grunt.registerTask('prod', ['uglify', 'concat']);
+};
